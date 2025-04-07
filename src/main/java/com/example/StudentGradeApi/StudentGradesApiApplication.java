70f9432e793a9a2ffec0c5a08febc1373e5527bd
imene <bentifraouineimene@gmail.com>
2025-04-07 10:43:20 +0200

    Initial commit

---
+++ src/main/java/com/example/StudentGradeApi/StudentGradesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentGradeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentGradesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudentGradeApi/StudentGradesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentGradeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
